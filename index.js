abbce8b6ad28291b9831ef85ba2381236c7855cb
Cleomir Machado Gomes <cleomir.mg@hotmail.com>
2024-03-14 21:51:21 -0300

    Add handlebars template engine and home page view

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require("express");
+const handleBars = require("express-handlebars");
 
 const logger = require("./middleware/logger");
-
+const users = require("./routes/users");
 const app = express();
 const port = 3000;
 
+// template engine
+app.engine("handlebars", handleBars.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+// middleware
 app.use(logger);
-
-const users = require("./routes/users");
-
 app.use(express.static("public"));
 
+// routes
+app.get("/", (req, res) => {
+  res.render("home");
+});
 app.use("/api", users);
 
 app.listen(port, () => {
